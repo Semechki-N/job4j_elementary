21b0cd559836afb1df7686e2c8a6664959560c1b
infer <infernusx1991@gmail.com>
2024-05-05 21:28:55 +0300

    3.2. Загрузка файлов на github [#243562 #77977]

---
+++ src/main/java/ru/job4j/condition/RectangleArea.java
@@ -0,0 +1,12 @@
+package ru.job4j.condition;
+
+public class RectangleArea {
+    public static double square(double p, double k) {
+        return (p / (2 * (k + 1))) * k;
+    }
+
+    public static void main(String[] args) {
+        double result = RectangleArea.square(4, 1);
+        System.out.println(" p = 4, k = 1, s = 1, real = " + result);
+    }
+}
