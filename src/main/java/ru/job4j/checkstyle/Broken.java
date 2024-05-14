284a625addf9ac19014bd89c1fa15fcb9c67be5b
infer <infernusx1991@gmail.com>
2024-05-14 13:40:01 +0300

    6. Правила Checkstyle [#504981]

---
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -0,0 +1,23 @@
+package ru.job4j.checkstyle;
+
+public class Broken {
+    private final int size = 10;
+    public static final String NEW_VALUE = "";
+    public String surname;
+    String name;
+
+    void broken() {
+    }
+
+    void echo() {
+    }
+
+    void media(Object obj) {
+        if (obj != null) {
+            System.out.println(obj);
+        }
+    }
+
+    void method(int age, int weight, int height) {
+    }
+}
