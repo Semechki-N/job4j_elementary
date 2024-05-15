c1ea648d1bf52b8338e65477d14331fb13dc41c3
infer <infernusx1991@gmail.com>
2024-05-15 18:07:59 +0300

    6. Правила Checkstyle [#504981]

--- src/main/java/ru/job4j/checkstyle/Broken.java
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -2,22 +2,24 @@ package ru.job4j.checkstyle;
 
 public class Broken {
     private final int size = 10;
-    public static final String NEW_VALUE = "";
-    public String surname;
-    String name;
+    private static final String NEW_VALUE = "";
+    private String surname;
+    private String name;
 
-    void broken() {
+    public Broken(int size, String surname, String name) {
+        this.name = name;
+        this.surname = surname;
     }
 
-    void echo() {
+    public void echo() {
     }
 
-    void media(Object obj) {
+    public void media(Object obj) {
         if (obj != null) {
             System.out.println(obj);
         }
     }
 
-    void method(int age, int weight, int height) {
+    public void method(int age, int weight, int height) {
     }
 }
